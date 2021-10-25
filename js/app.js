c6c9813721042a282a98d1016a2dc042c53d5295
Shaun Pour <scpour@gmail.com>
2021-10-25 10:12:33 -0400

    1.00

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let getList = document.querySelector(".list").value;
+const listAdd = document.querySelector(".list-add");
+const button = document.querySelector(".button");
+
+
+function addToList() {
+    console.log(getList);
+}
